e0b81db00f0e6fd771a2097e090d740db5e5407e
kiiilllerbot <dev.shimolkhan@gmail.com>
2024-07-14 00:38:24 +0800

    Initial Commit

---
+++ TodoX/TodoXApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoXApp.swift
+//  TodoX
+//
+//  Created by Shimol Khan on 14/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
