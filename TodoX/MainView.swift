5f09af01075845403e547cdd537dbecb65fc46d5
kiiilllerbot <dev.shimolkhan@gmail.com>
2024-07-14 22:00:46 +0800

    Integrated Sign In View.

--- TodoX/MainView.swift
+++ TodoX/MainView.swift
@@ -5,21 +5,11 @@
 //  Created by Shimol Khan on 14/07/2024.
 //
 
-import FirebaseCore
 import SwiftUI
 
 struct MainView: View {
-    init(){
-        FirebaseApp.configure()
-    }
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        LogInView()
     }
 }
 
--- TodoX/Views/LogInView.swift
+++ TodoX/Views/LogInView.swift
@@ -8,8 +8,45 @@
 import SwiftUI
 
 struct LogInView: View {
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                // Header
+                HeaderView()
+                // Login Form
+                Form {
+                    TextField("Email Address", text: $email)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    Button{
+                        
+                    }label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(.pink)
+                            
+                            Text("Log In")
+                                .foregroundColor(.white)
+                                .bold()
+                        }
+                    }
+                }
+                
+                // Sign Up
+                VStack {
+                    Text("New around here?")
+                    NavigationLink("Create An Account", destination: SignUpView())
+                }
+            }
+            .padding(.bottom, 50)
+            
+            Spacer()
+        }
     }
 }
 
